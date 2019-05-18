97a43f44906fdb0053390f8c5a501d000c70950a
Max Wittal <automyinc@gmail.com>
2019-05-18 05:41:12 +0200

    added cross()

--- include/automy/math/Math3D.h
+++ include/automy/math/Math3D.h
@@ -160,6 +160,13 @@ Matrix<T, 3, 1> matrix_to_euler_xyz(const Matrix<T, 3, 3>& matrix) {
 	return rpy;
 }
 
+template<typename T>
+Matrix<T, 3, 1> cross(const Matrix<T, 3, 1>& A, const Matrix<T, 3, 1>& B) {
+	return Matrix<T, 3, 1>({A[1]*B[2] - A[2]*B[1],
+							A[2]*B[0] - A[0]*B[2],
+							A[0]*B[1] - A[1]*B[0]});
+}
+
 
 } // math
 } // automy
