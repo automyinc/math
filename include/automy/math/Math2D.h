01cd1a5ef6401f741e3e709bb265a68b728f712d
Max Wittal <wittal@neobotix.de>
2020-06-08 14:26:38 +0200

    added 2.5D functions

--- include/automy/math/Math2D.h
+++ include/automy/math/Math2D.h
@@ -43,6 +43,61 @@ Matrix<T, 3, 3> scale2(T x, T y) {
 			0, 0, 1};
 }
 
+/*
+ * Creates a 2D (x, y, yaw) transformation matrix.
+ */
+template<typename T>
+Matrix<T, 4, 4> transform2(T x, T y, T rad) {
+	return translate2(x, y) * rotate2(rad);
+}
+
+/*
+ * Creates a 2D (x, y, yaw) transformation matrix.
+ */
+template<typename T>
+Matrix<T, 4, 4> transform2(const Matrix<T, 3, 1>& pose) {
+	return translate2(pose[0], pose[1]) * rotate2(pose[2]);
+}
+
+
+/*
+ * Creates a 2.5D (x, y, yaw) rotation matrix.
+ */
+template<typename T>
+Matrix<T, 4, 4> rotate25(T rad) {
+	return {std::cos(rad), -std::sin(rad), 0, 0,
+			std::sin(rad), std::cos(rad), 0, 0,
+			0, 0, 1, rad,
+			0, 0, 0, 1};
+}
+
+/*
+ * Creates a 2.5D (x, y, yaw) translation matrix.
+ */
+template<typename T>
+Matrix<T, 4, 4> translate25(T x, T y) {
+	return {1, 0, 0, x,
+			0, 1, 0, y,
+			0, 0, 1, 0,
+			0, 0, 0, 1};
+}
+
+/*
+ * Creates a 2.5D (x, y, yaw) transformation matrix.
+ */
+template<typename T>
+Matrix<T, 4, 4> transform25(T x, T y, T rad) {
+	return translate25(x, y) * rotate25(rad);
+}
+
+/*
+ * Creates a 2.5D (x, y, yaw) transformation matrix.
+ */
+template<typename T>
+Matrix<T, 4, 4> transform25(const Matrix<T, 3, 1>& pose) {
+	return translate25(pose[0], pose[1]) * rotate25(pose[2]);
+}
+
 
 } // math
 } // automy
