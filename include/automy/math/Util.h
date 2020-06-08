592f2564cdff3eee351515fbd5ea79429800d58e
Max Wittal <wittal@neobotix.de>
2020-06-08 17:15:34 +0200

    added normalize_angle functions

--- include/automy/math/Util.h
+++ include/automy/math/Util.h
@@ -11,6 +11,32 @@ namespace automy {
 namespace math {
 
 /*
+ * Normalize angle in [rad] between +pi and -pi.
+ */
+template<typename T>
+T normalize_angle(T rad) {
+	T angle = std::modf(rad, T(2 * M_PI));
+	if(angle > M_PI) {
+		angle -= T(2 * M_PI);
+	} else if(angle < -M_PI) {
+		angle += T(2 * M_PI);
+	}
+	return angle;
+}
+
+/*
+ * Normalize angle in [rad] between 0 and 2 * pi.
+ */
+template<typename T>
+T normalize_angle_positive(T rad) {
+	T angle = std::modf(rad, T(2 * M_PI));
+	if(angle < 0) {
+		angle += T(2 * M_PI);
+	}
+	return angle;
+}
+
+/*
  * Computes angle difference as a - b in radians.
  */
 template<typename T>
@@ -19,10 +45,10 @@ T angle_diff(T a, T b) {
 }
 
 /*
- * Computes angle difference as a - b in radians.
+ * Computes 3D angle difference as a - b in radians.
  */
 template<typename T>
-Matrix<T,3,1> angle_diff(const Matrix<T,3,1>& a, const Matrix<T,3,1>& b) {
+Matrix<T, 3, 1> angle_diff(const Matrix<T, 3, 1>& a, const Matrix<T, 3, 1>& b) {
 	return {angle_diff(a[0], b[0]), angle_diff(a[1], b[1]), angle_diff(a[2], b[2])};
 }
 
